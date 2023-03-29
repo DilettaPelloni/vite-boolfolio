bed5a407cc67d08e5d2b3ac447c5fa64b8fd0986
Diletta Pelloni <dilpel@live.it>
2023-03-29 17:13:41 +0200

    fatto bonus

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -7,6 +7,8 @@
         data() {
             return {
                 projects: [],
+                currentPage: 1,
+                lastPage: 1,
             }
         }, //data
         components: {
@@ -14,11 +16,31 @@
         }, //components
         methods: {
             getProjects() {
-                axios.get('http://127.0.0.1:8000/api/projects')
-                    .then((response)=> {
-                        this.projects = response.data.projects;
+                axios.get('http://127.0.0.1:8000/api/projects',{
+                    params: {
+                        page: this.currentPage,
+                    }
+                }).then((response)=> {
+                        this.projects = response.data.projects.data;
+                        this.lastPage = response.data.projects.last_page;
                     })
             }, //getProjects
+            changePage(page) {
+                this.currentPage = page;
+                this.getProjects();
+            }, //changePage
+            prevPage(){
+                if(this.currentPage > 1) {
+                    this.currentPage --;
+                    this.getProjects();
+                }
+            }, //prevPage
+            nextPage(){
+                if(this.currentPage != this.lastPage) {
+                    this.currentPage ++;
+                    this.getProjects();
+                }
+            }, //nextPage
         }, //methods
         created() {
             this.getProjects();
@@ -27,13 +49,51 @@
 </script>
 
 <template>
-    <div class="container py-5">
+    <div class="container py-5 vh-100">
         <h1>Progetti</h1>
-        <div class="row g-4 mt-3">
+        <div class="row g-4 mt-3 h-50">
             <div class="col" v-for="project in projects">
                 <ProjectCard :project="project" class="h-100"/>
             </div>
         </div>
+        <nav class="mt-5">
+            <ul class="pagination justify-content-center">
+                <li
+                    class="page-item"
+                    :class="{disabled: currentPage == 1}"
+                >
+                    <button
+                        class="page-link"
+                        @click="prevPage"
+                    >
+                        Previous
+                    </button>
+                </li>
+                <li
+                    class="page-item"
+                    :class="{active: i == currentPage}"
+                    v-for="i in lastPage"
+                >
+                    <button
+                        class="page-link"
+                        @click="changePage(i)"
+                    >
+                        {{ i }}
+                    </button>
+                </li>
+                <li
+                    class="page-item"
+                >
+                    <button
+                        class="page-link"
+                        :class="{disabled: currentPage == lastPage}"
+                        @click="nextPage"
+                    >
+                        Next
+                    </button>
+                </li>
+            </ul>
+        </nav>
     </div>
 </template>
 
