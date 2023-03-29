64b966cf16087dd0994194b072934b4c92a559e4
Diletta Pelloni <dilpel@live.it>
2023-03-29 15:52:36 +0200

    inizializzazione progetto

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+    import axios from 'axios';
+	export default {
+		name:'App',
+	};//export
+</script>
+
+<template>
+</template>
+
+<style lang="scss">
+@import "./style/main.scss";
+
+</style>
