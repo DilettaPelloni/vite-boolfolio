b222338391d43f2dbf951e8b88aa4f01bfd8b5ff
Diletta Pelloni <dilpel@live.it>
2023-03-29 16:45:28 +0200

    esercizio completato

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,12 +1,22 @@
 <script>
     import axios from 'axios';
+    import ProjectCard from './ProjectCard.vue';
+
 	export default {
 		name:'AppMain',
+        data() {
+            return {
+                projects: [],
+            }
+        }, //data
+        components: {
+            ProjectCard,
+        }, //components
         methods: {
             getProjects() {
                 axios.get('http://127.0.0.1:8000/api/projects')
                     .then((response)=> {
-                        console.log(response.data);
+                        this.projects = response.data.projects;
                     })
             }, //getProjects
         }, //methods
@@ -17,7 +27,14 @@
 </script>
 
 <template>
-    prova
+    <div class="container py-5">
+        <h1>Progetti</h1>
+        <div class="row g-4 mt-3">
+            <div class="col" v-for="project in projects">
+                <ProjectCard :project="project" class="h-100"/>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
